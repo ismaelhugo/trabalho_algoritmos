f9dec0fddb6b79d353f3d96d0296ecf54eeae99c
sanderson <sandbar.dev@gmail.com>
2021-05-01 22:09:14 -0300

    algoritmo 1 completo

---
+++ gerador_lista.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUMS_NEEDED 1000000
+
+int main()
+{
+    int sizeArray = 0;
+    int i = 0, j = 0;
+    int nums[NUMS_NEEDED];
+    FILE *fp = NULL;
+
+    srand( time( NULL ) );
+    fp = fopen( "lista.txt", "w" );
+
+    if( fp == NULL )
+    {
+        printf( "erro.\n" );
+        return 1;
+    }
+    while( sizeArray < NUMS_NEEDED )
+    {
+        int numGenerated = 1 + rand()% 9999999;
+        // Verifica se o número já existe
+        for( i = 0 ; i < sizeArray ; ++i )
+        {
+            if( nums[i] == numGenerated )
+            {
+                break;
+            }
+        }
+        if( i == sizeArray )
+        {
+            fprintf( fp, "%d\n", numGenerated );
+            nums[++sizeArray] = numGenerated;
+        }
+    }
+
+    fclose( fp );
+    fp = NULL;
+    return 0;
+}
+
+// algoritmo gerador de numeros aleatorios sem repetição
+// copiado da internet <3
---
+++ lista_simples/1_listasimples_busca.c
@@ -0,0 +1,54 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int buscar(int *lista, int tamanho, int valor_buscado) {
+   int i;
+
+   for (i = 0; i < tamanho; i++) {
+        if ( lista[i] == valor_buscado) {
+	        return i;
+	    }
+   }
+
+   if (i == tamanho) {
+        return -1;
+   }
+}
+
+int main() {
+    FILE *arq;
+    clock_t clock_inicial, clock_final;
+    int *lista;
+    int contador, valor_buscado, busca;
+    long int numero;
+
+    // alocação para uma quantidade máxima de 1 milhão de ints na lista
+    lista = malloc(1000000*sizeof(int));
+
+    // leitura dos dados em disco (arquivo) para um vetor
+    arq = fopen("lista.txt", "r");
+    contador = 0;
+    while (fscanf(arq, "%ld", &numero) > 0) {
+        lista[contador] = numero;
+	    contador++;
+    }
+    fclose(arq);
+    // fecha o arquivo ao fim da leitura
+
+    // define valor a ser buscado e executa o algoritmo
+    printf("Digite um numero para buscar: ");
+    scanf("%d", &valor_buscado);
+
+    clock_inicial = clock();
+    busca = buscar(lista, contador, valor_buscado);
+    clock_final = clock();
+
+    // se o retorno da busca for um índice negativo, o valor não está presente na lista
+    if (busca == -1) printf("Valor não encontrado!\n");
+    else printf("Valor encontrado na posição %d.\n", busca + 1);
+
+    printf("Tempo decorrido: %lfs. \n", ((float)(clock_final - clock_inicial) / CLOCKS_PER_SEC));
+
+    return 0;
+}
